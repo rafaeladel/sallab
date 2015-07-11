6f3c9c31c4bc2fbc74613118fa583c98889981bf
minaalfy <minaalfy@github.com>
2015-07-11 16:57:16 +0200

    frontend

--- app/controllers/frontend/frontend_home_controller.rb
+++ app/controllers/frontend/frontend_home_controller.rb
@@ -4,6 +4,5 @@ class Frontend::FrontendHomeController < ApplicationController
 	def index 
 		@banner = Banner.find_by(banner_type: Banner.banner_types[:home])
 		@product_sections = ProductSection.all
-		render "frontend/frontend_home.html.erb"
 	end
 end
