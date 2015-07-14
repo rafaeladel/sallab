526d62f6ced0f62b6ad17843cc94c668d4a71da4
rafael <rafael.adel20@gmail.com>
2015-07-15 00:02:56 +0200

    galleries and news

--- app/controllers/frontend/frontend_gallery_controller.rb
+++ app/controllers/frontend/frontend_gallery_controller.rb
@@ -1,5 +1,6 @@
 class Frontend::FrontendGalleryController < Frontend::FrontendApplicationController 
-	def index 
-		@banner = Banner.find_by(banner_type: Banner.banner_types[:home])		
+	def index
+		@banner = Banner.find_by(banner_type: Banner.banner_types[:home])
+		@galleries = SevenGallery::Gallery.where(banner: nil, seven_portfolio_item_id: nil)
 	end
 end
--- app/controllers/frontend/frontend_news_controller.rb
+++ app/controllers/frontend/frontend_news_controller.rb
@@ -1,5 +1,6 @@
 class Frontend::FrontendNewsController < Frontend::FrontendApplicationController 
 	def index 
 		@banner = Banner.find_by(banner_type: Banner.banner_types[:news])
+		@news_items = SevenPortfolio::Item.page(params[:page]).per(7)
 	end
 end
