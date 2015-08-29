3a27b652e4142f4c847e8a9b51e0ec9528b4ed85
rafael <rafael.adel20@gmail.com>
2015-08-29 23:50:45 +0300

    added mail conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -78,4 +78,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.mandrillapp.com",
+      :port                 => 587,
+      :user_name            => "sallab.staff.test@hotmail.com",
+      :password             => "pWmYZOsuBAioP9Vo57QL4Q",
+      :authentication       => "login",
+      :enable_starttls_auto => true
+  }
 end
